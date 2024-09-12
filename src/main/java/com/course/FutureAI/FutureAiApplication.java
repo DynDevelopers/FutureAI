9134e674e71bdcfacc6e4cb74662d3dec0a04cf7
HarrisNaw <mohd.nawarangy@ucdconnect.ie>
2024-09-12 09:18:28 +0100

    Starter Application
    Spring boot MVC application for machine learning courses

---
+++ src/main/java/com/course/FutureAI/FutureAiApplication.java
@@ -0,0 +1,13 @@
+package com.course.FutureAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutureAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutureAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/course/FutureAI/FutureAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course.FutureAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutureAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
